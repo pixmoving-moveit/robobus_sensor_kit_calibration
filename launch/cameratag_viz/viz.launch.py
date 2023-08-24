d0b8612417016a624db786d0d8b85435a8174fed
zymouse <zymouse@pixmoving.net>
2023-08-24 18:18:20 +0800

    fix:flir usb_cam

--- launch/cameratag_viz/viz.launch.py
+++ launch/cameratag_viz/viz.launch.py
@@ -3,10 +3,15 @@ from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 
 def generate_launch_description():
+    def add_launch_arg(name: str, default_value=None):
+        launch_arguments.append(DeclareLaunchArgument(name, default_value=default_value))
+
+    add_launch_arg("image_topic", "/sensing/camera/traffic_light/flir_camera/image_raw")
+    add_launch_arg("detections", "/sensor_kit/sensor_kit_base_link/traffic_light_left_camera/camera_link/apriltag/detection_array")
+
     composable_node = ComposableNode(name='viz', package='apriltag_viz', plugin='AprilVizNode',
-                        remappings=[("image", "/sensing/camera/traffic_light/flir_camera/rectified/image_raw"),
-                                    ("detections", "/sensor_kit/sensor_kit_base_link/traffic_light_left_camera/camera_link/apriltag/detection_array")
-                                #     ("detections", "/apriltag/detection_array")
+                        remappings=[("image", LaunchConfiguration("image_topic")),
+                                    ("detections", LaunchConfiguration("detections"))
                                     ],)
     container = ComposableNodeContainer(
             name='viz_container',
