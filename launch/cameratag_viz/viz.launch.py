cebccc8c7953683d9575d9a072eddc572eec9211
zymouse <zymouse@pixmoving.net>
2023-08-26 16:10:31 +0800

    feat：启动传感器

--- launch/cameratag_viz/viz.launch.py
+++ launch/cameratag_viz/viz.launch.py
@@ -1,13 +1,19 @@
 import launch
 from launch_ros.actions import ComposableNodeContainer
+from launch.substitutions import LaunchConfiguration
+
 from launch_ros.descriptions import ComposableNode
+from launch.actions import DeclareLaunchArgument
+
 
 def generate_launch_description():
+    launch_arguments = []
+
     def add_launch_arg(name: str, default_value=None):
         launch_arguments.append(DeclareLaunchArgument(name, default_value=default_value))
 
-    add_launch_arg("image_topic", "/sensing/camera/traffic_light/flir_camera/image_raw")
-    add_launch_arg("detections", "/sensor_kit/sensor_kit_base_link/traffic_light_left_camera/camera_link/apriltag/detection_array")
+    add_launch_arg("image_topic", "/sensing/camera/traffic_light/rectified/image_raw")
+    add_launch_arg("detections", "/apriltag/detection_array")
 
     composable_node = ComposableNode(name='viz', package='apriltag_viz', plugin='AprilVizNode',
                         remappings=[("image", LaunchConfiguration("image_topic")),
@@ -19,8 +25,8 @@ def generate_launch_description():
             package='rclcpp_components',
             executable='component_container',
             composable_node_descriptions=[composable_node],
-            remappings=[("/apriltag/image", "/sensing/camera/traffic_light/flir_camera/image_raw/decompressed")],
+            remappings=[("/apriltag/image", "/sensing/camera/traffic_light/image_raw/decompressed")],
             output='screen'
     )
 
-    return launch.LaunchDescription([container])
+    return launch.LaunchDescription(launch_arguments+[container])
