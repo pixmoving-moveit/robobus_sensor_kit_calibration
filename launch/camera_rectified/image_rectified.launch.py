7aa297cd5647d2a6ce4d272dae99cd2fd57f42a5
zymouse <zymouse@pixmoving.net>
2023-09-01 17:53:34 +0800

    fix: 相机内参验证可视化

--- launch/camera_rectified/image_rectified.launch.py
+++ launch/camera_rectified/image_rectified.launch.py
@@ -7,12 +7,12 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     node = Node(
-      package='output_rectified_image',
-      executable='output_rectified_image_node',
-      name='output_rectified_image_node',
+      package='output_rectified_image_py',
+      executable='output_rectified_image_py_node',
+      name='output_rectified_image_py_node',
       remappings=[
-        ('input_image_raw', '/sensing/camera/traffic_light/image_raw'),
-        ('input_camera_info', '/sensing/camera/traffic_light/camera_info'),
+        ('input_image_raw', '/image_raw'),
+        ('input_camera_info', '/camera_info'),
 
         ('output_image_raw', '/sensing/camera/traffic_light/rectified/image_raw'),
         ('output_camera_info', '/sensing/camera/traffic_light/rectified/camera_info')],
