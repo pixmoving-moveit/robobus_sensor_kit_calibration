6ede38268d3329eaf467a74d24e291239f3c391f
zymouse <zymouse@pixmoving.net>
2023-09-04 10:33:52 +0800

    fix: 相机内参角点检测

--- launch/camera_rectified/image_rectified.launch.py
+++ launch/camera_rectified/image_rectified.launch.py
@@ -3,18 +3,30 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
 
 
 def generate_launch_description():
+    launch_arguments = []
+
+    def add_launch_arg(name: str, default_value=None):
+        launch_arguments.append(DeclareLaunchArgument(name, default_value=default_value))
+
+    add_launch_arg("input_image_raw", "/sensing/camera/traffic_light/image_raw")
+    add_launch_arg("input_camera_info", "/sensing/camera/traffic_light/camera_info")
+    add_launch_arg("output_image_raw", "/sensing/camera/traffic_light/rectified/image_raw")
+    add_launch_arg("output_camera_info", "/sensing/camera/traffic_light/rectified/camera_info")
     node = Node(
       package='output_rectified_image_py',
       executable='output_rectified_image_py_node',
       name='output_rectified_image_py_node',
       remappings=[
-        ('input_image_raw', '/image_raw'),
-        ('input_camera_info', '/camera_info'),
+        ('input_image_raw', LaunchConfiguration("input_image_raw")),
+        ('input_camera_info', LaunchConfiguration("input_camera_info")),
 
-        ('output_image_raw', '/sensing/camera/traffic_light/rectified/image_raw'),
-        ('output_camera_info', '/sensing/camera/traffic_light/rectified/camera_info')],
+        ('output_image_raw', LaunchConfiguration("output_image_raw")),
+        ('output_camera_info', LaunchConfiguration("output_camera_info"))],
       output='screen')
-    return LaunchDescription([node])
+    return LaunchDescription(launch_arguments+[node,])
