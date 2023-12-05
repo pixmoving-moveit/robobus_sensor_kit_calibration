4664c787573448319fbce0300c898755ed5eba9f
zymouse <zymouse@pixmoving.net>
2023-12-05 15:56:24 +0800

    fix:测试通过

---
+++ launch/sensing_launch/camera/camera_usb_camera_top.launch.py
@@ -0,0 +1,87 @@
+# Copyright 2018 Lucas Walter
+# All rights reserved.
+#
+# Software License Agreement (BSD License 2.0)
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Lucas Walter nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+import argparse
+import os
+import sys
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    parser = argparse.ArgumentParser(description='usb_cam launch')
+    parser.add_argument('-n', '--node-name', dest='node_name', type=str,
+                        help='name for device', default='usb_cam')
+
+    args, unknown = parser.parse_known_args(sys.argv[4:])
+  
+    node_name = args.node_name
+    # get path to params file
+    parameters={
+        "video_device": "/dev/camera/top_6mm",
+        "framerate": 30.0,
+        "io_method": "mmap",
+        "frame_id": "camera_front_link",
+        "pixel_format": "yuyv2rgb",
+        "image_width": 1920 ,
+        "image_height": 1080,
+        "camera_name": "gmsl_camera",
+        "camera_info_url": "file://${ROS_HOME}/../pix/parameter/sensor_kit/robobus_sensor_kit_description/intrinsic_parameters/camera_top_6mm.yaml",
+        # reusing same camera intrinsics only for demo, should really be unique for camera2"
+        "brightness": -1,
+        "contrast": -1,
+        "saturation": -1,
+        "sharpness": -1,
+        "gain": -1,
+        "auto_white_balance": True,
+        "white_balance": 4000,
+        "autoexposure": True,
+        "exposure": 100,
+        "autofocus": False,
+        "focus": -1,
+        # resize
+        "image_resize": 2,
+        "camera_lifetime": 33,
+    }
+    parameters["video_device"] = os.path.realpath(parameters["video_device"])
+    ld.add_action(Node(
+        package='usb_cam', executable='usb_cam_node_exe', output='screen',
+        name=node_name,
+        namespace="top/gmsl",
+        parameters=[parameters]
+        ))
+    return ld
---
+++ launch/sensing_launch/camera/spinnaker_camera_driver.launch.py
@@ -0,0 +1,145 @@
+# -----------------------------------------------------------------------------
+# Copyright 2022 Bernd Pfrommer <bernd.pfrommer@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+
+from launch.substitutions import LaunchConfiguration as LaunchConfig
+from launch.substitutions import PathJoinSubstitution
+from launch.actions import DeclareLaunchArgument as LaunchArg
+from launch.actions import OpaqueFunction
+from launch import LaunchDescription
+
+example_parameters = {
+    'blackfly_s': {
+        'debug': False,
+        'compute_brightness': False,
+        'adjust_timestamp': True,
+        'dump_node_map': False,
+        # set parameters defined in blackfly_s.yaml
+        'gain_auto': 'Continuous',
+        # 'pixel_format': 'BayerRG8',
+        'exposure_auto': 'Continuous',
+        # These are useful for GigE cameras
+        # 'device_link_throughput_limit': 380000000,
+        # 'gev_scps_packet_size': 9000,
+        # ---- to reduce the sensor width and shift the crop
+        # 'image_width': 1408,
+        # 'image_height': 1080,
+        # 'offset_x': 16,
+        # 'offset_y': 0,
+        'frame_rate_auto': 'Off',
+        'frame_rate': 40.0,
+        'frame_rate_enable': True,
+        'buffer_queue_size': 1,
+        'trigger_mode': 'Off',
+        'chunk_mode_active': True,
+        'chunk_selector_frame_id': 'FrameID',
+        'chunk_enable_frame_id': False,
+        'chunk_selector_exposure_time': 'ExposureTime',
+        'chunk_enable_exposure_time': True,
+        'chunk_selector_gain': 'Gain',
+        'chunk_enable_gain': True,
+        'chunk_selector_timestamp': 'Timestamp',
+        'chunk_enable_timestamp': True},
+    'chameleon': {
+        'debug': False,
+        'compute_brightness': False,
+        'dump_node_map': False,
+        # set parameters defined in chameleon.yaml
+        'gain_auto': 'Continuous',
+        'exposure_auto': 'Continuous',
+        'offset_x': 0,
+        'offset_y': 0,
+        'image_width': 2048,
+        'image_height': 1536,
+        'pixel_format': 'RGB8',  # 'BayerRG8, 'RGB8' or 'Mono8'
+        'frame_rate_continous': True,
+        'frame_rate': 100.0,
+        'trigger_mode': 'Off',
+        'chunk_mode_active': True,
+        'chunk_selector_frame_id': 'FrameID',
+        'chunk_enable_frame_id': True,
+        'chunk_selector_exposure_time': 'ExposureTime',
+        'chunk_enable_exposure_time': True,
+        'chunk_selector_gain': 'Gain',
+        'chunk_enable_gain': True,
+        'chunk_selector_timestamp': 'Timestamp',
+        'chunk_enable_timestamp': True},
+    'grasshopper': {
+        'debug': False,
+        'compute_brightness': False,
+        'dump_node_map': False,
+        # set parameters defined in grasshopper.yaml
+        'gain_auto': 'Continuous',
+        'exposure_auto': 'Continuous',
+        'frame_rate_auto': 'Off',
+        'frame_rate': 100.0,
+        'trigger_mode': 'Off',
+        'chunk_mode_active': True,
+        'chunk_selector_frame_id': 'FrameID',
+        'chunk_enable_frame_id': True,
+        'chunk_selector_exposure_time': 'ExposureTime',
+        'chunk_enable_exposure_time': True,
+        'chunk_selector_gain': 'Gain',
+        'chunk_enable_gain': True,
+        'chunk_selector_timestamp': 'Timestamp',
+        'chunk_enable_timestamp': True}
+    }
+
+
+def launch_setup(context, *args, **kwargs):
+    """Launch camera driver node."""
+    # parameter_file = LaunchConfig('parameter_file').perform(context)
+    # camera_type = LaunchConfig('camera_type').perform(context)
+    serial_number = "22485232"
+    camera_type = 'blackfly_s'
+    parameter_file = PathJoinSubstitution(
+        [FindPackageShare('robobus_sensor_kit_calibration'), 'launch', 'sensing_launch', 'camera'
+            camera_type + '.yaml'])
+    
+    camerainfo_url = 'file://${ROS_HOME}/../pix/parameter/sensor_kit/robobus_sensor_kit_description/intrinsic_parameters/camera_top_12mm.yaml'
+    
+    node = Node(package='spinnaker_camera_driver',
+                executable='camera_driver_node',
+                output='screen',
+                name="flir_12mm",
+                parameters=[example_parameters[camera_type],
+                            {'parameter_file': parameter_file,
+                             'frame_id': 'camera_top',
+                             'serial_number': serial_number,
+                             'camerainfo_url': camerainfo_url
+                             }
+                            ],
+                )
+
+    return [node]
+
+
+def generate_launch_description():
+    """Create composable node by calling opaque function."""
+    return LaunchDescription([
+        # LaunchArg('camera_name', default_value=['flir_camera'],
+        #           description='camera name (ros node name)'),
+        LaunchArg('camera_type', default_value='blackfly_s',
+                  description='type of camera (blackfly_s, chameleon...)'),
+        LaunchArg('serial', default_value="'20435008'",
+                  description='FLIR serial number of camera (in quotes!!)'),
+        LaunchArg('parameter_file', default_value='22495519',
+                  description='path to ros parameter definition file (override camera type)'),
+        OpaqueFunction(function=launch_setup)
+        ])
